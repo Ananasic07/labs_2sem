1de183c635be71b3280e1fa0bb25fa6439acc0d4
Ananasic07 <106329801+Ananasic07@users.noreply.github.com>
2024-05-21 23:12:38 +0500

    Add files via upload

---
+++ поиски/бинарный поиск/Линейный поиск/Линейный поиск/Линейный поиск.cpp
@@ -0,0 +1,35 @@
+﻿// Линейный поиск.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+//линейный поиск
+int linSearch(int arr[], int Size, int target)
+{
+	for (int i = 0; i < Size; i++)
+	{
+		if (arr[i] == target)
+			return i;
+	}
+	return -1;
+}
+
+int main() {
+    system("chcp 1251");
+    int array[] = { 6, 2, 8, 4, 5, 1, 7, 3 };
+    int size = 8;
+    int target = 1;
+
+    int result = linSearch(array, size, target);
+
+    if (result == -1) {
+        cout << "Элемент не найден"<<endl;
+    }
+    else {
+        cout << "Элемент найден в позиции " << result << endl;
+    }
+
+    return 0;
+}
---
+++ поиски/бинарный поиск/поиск подстроки в строке/поиск подстроки в строке/поиск подстроки в строке.cpp
@@ -0,0 +1,37 @@
+﻿// поиск подстроки в строке.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>;
+using namespace std;
+
+int main() {
+	system("chcp 1251");
+	string str = "abcababa";
+	string substr = "ba";
+
+	int lenstr = str.length();
+	int lensubstr = substr.length();
+
+	int foundIndex = -1;
+
+	for (int i = 0; i<=lenstr-lensubstr; i++) {
+		bool flag = true;
+		for (int j = 0; j < lensubstr; j++) {
+			if (str[i + j] != substr[j]) {
+				flag = false;
+				break;
+			}
+		}
+		if (flag) {
+			foundIndex = i;
+			break;
+		}
+	}
+	if (foundIndex != -1) {
+		cout << "Подстрока найдена на позиции " << foundIndex << endl;
+	}
+	else {
+		cout << "Подстрока не найдена (" << endl;
+	}
+	return 0;
+}
